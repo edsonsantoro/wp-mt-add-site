1ee90b2149b10b6e890b29a9d79c4a1c5f4d90b0
Edson Del Santoro <edsonsantoro@gmail.com>
2021-09-12 15:30:09 +0900

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,20 +2,20 @@
 # Modifying .env
 
 if [[ $1 ]]; then
-    sed -i 's/WP_ENV=/WP_ENV=$1/g' ./.env
+    sed -i 's/WP_ENV=/WP_ENV='$1'/g' .env
 fi
 if [[ $2 ]]; then
-    sed -i 's/https://www.example.com/$2/g' ./.env
+    sed -i 's/www.example.com/'$2'/g' .env
 fi
 if [[ $3 ]]; then
-    sed -i 's/database_name_here/$3/g' ./.env
+    sed -i 's/database_name_here/'$3'/g' .env
 fi
 if [[ $4 ]]; then
-    sed -i 's/username_here/$4g' ./.env
+    sed -i 's/username_here/'$4'/g' .env
 fi
 if [[ $5 ]]; then
-    sed -i 's/password_here/$5/g' ./.env
+    sed -i 's/password_here/'$5'/g' .env
 fi
 if [[ $6 ]]; then
-    sed -i 's/table_prefix/$6/g' ./.env
+    sed -i 's/wp_/'$6'/g' .env
 fi
