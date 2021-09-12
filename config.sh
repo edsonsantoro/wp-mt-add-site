3a4c35c2eb26b0c8d748ae228b696bd98beeebc3
Edson Del Santoro <edsonsantoro@gmail.com>
2021-09-12 15:11:03 +0900

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Modifying .env
+
+if [[ $1 ]]; then
+    sed -i 's/WP_ENV=/WP_ENV=$1/g' ./.env
+fi
+if [[ $2 ]]; then
+    sed -i 's/https://www.example.com/$2/g' ./.env
+fi
+if [[ $3 ]]; then
+    sed -i 's/database_name_here/$3/g' ./.env
+fi
+if [[ $4 ]]; then
+    sed -i 's/username_here/$4g' ./.env
+fi
+if [[ $5 ]]; then
+    sed -i 's/password_here/$5/g' ./.env
+fi
+if [[ $6 ]]; then
+    sed -i 's/table_prefix/$6/g' ./.env
+fi
