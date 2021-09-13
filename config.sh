64b2b9242429c7d40fb60b20cc64587deadc52b2
Edson Del Santoro <edsonsantoro@gmail.com>
2021-09-13 20:18:51 +0900

    Update config.sh
    Reorganized

--- config.sh
+++ config.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 # Modifying .env
 
-if [[ $1 ]]; then
+if [[ $1 ]] && [[ $2 ]] && [[ $3 ]] && [[ $4 ]] && [[ $5 ]] && [[ $6 ]]; then
     sed -i 's/WP_ENV=/WP_ENV='$1'/g' .env
-fi
-if [[ $2 ]]; then
     sed -i 's/www.example.com/'$2'/g' .env
-fi
-if [[ $3 ]]; then
     sed -i 's/database_name_here/'$3'/g' .env
-fi
-if [[ $4 ]]; then
     sed -i 's/username_here/'$4'/g' .env
-fi
-if [[ $5 ]]; then
     sed -i 's/password_here/'$5'/g' .env
-fi
-if [[ $6 ]]; then
     sed -i 's/wp_/'$6'/g' .env
 fi
